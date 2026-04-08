baf83a5ca2a108a76bf872d6511b0f268d3affa8
erlmek <erlm@ek.dk>
2026-04-08 08:34:05 +0200

    nu med roller

--- src/main/java/org/example/springsecurityf25b/controller/UserController.java
+++ src/main/java/org/example/springsecurityf25b/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,6 @@ public class UserController {
 
     @Autowired
     PasswordEncoder passwordEncoder;
-
 
     @PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
