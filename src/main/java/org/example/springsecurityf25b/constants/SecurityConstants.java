89796140eac30f9d8eed7277ac0fd1896ce075a6
phaberen <magnusphaberen@gmail.com>
2026-04-08 13:14:49 +0200

    so far progress of tutorial

---
+++ src/main/java/org/example/springsecurityf25b/constants/SecurityConstants.java
@@ -0,0 +1,4 @@
+package org.example.springsecurityf25b.constants;
+
+public interface SecurityConstants {
+}
---
+++ src/main/java/org/example/springsecurityf25b/filter/JWTTokenGeneratorFilter.java
@@ -0,0 +1,4 @@
+package org.example.springsecurityf25b.filter;
+
+public class JWTTokenGeneratorFilter {
+}
